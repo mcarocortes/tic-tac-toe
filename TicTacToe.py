43fc2cae44f9273109aac294af9da9478b37edf1
Macarena Caro <m.caro.cortes2@gmail.com>
2024-10-05 12:31:33 +0200

    funcion def Jugada

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,11 +1,12 @@
-varJugador = "x"
+from random import randrange
+varJugador = "X"
 lugar = 5
 winner = "nadie"
 resultado = []
 jugada = 0
 
 
-# Pinta tablero Inicial: Crea Tabla resultados
+# 1.Pinta tablero Inicial: Crea Tabla resultados
 def inicio():  
    contador = 0  
    for i in range(3):
@@ -13,7 +14,7 @@ def inicio():
         for j in range(3):
             contador += 1
             if contador == 5:
-                list.append("x")
+                list.append(varJugador)
             else:
               list.append(contador)
             if contador % 3 == 0:
@@ -21,7 +22,7 @@ def inicio():
 
 inicio()
 
-#Pinta Tablas nuevas despues de cada jugada
+# 2.Pinta Tablas nuevas despues de cada jugada
 def pinta():
     for fila in range(len(resultado)):
         print("+-------+-------+-------+")
@@ -32,10 +33,40 @@ def pinta():
 
 pinta()
 
-#Aquí se pregunta al usuario la jugada y se evalua si se puede
+#3.Aquí se pregunta al usuario la jugada y se evalua si se puede
 def jugada():
     intento = False
     jugada += 1
+    index
+    pos
 
-    def check(numero):#esta funcion es para ver si el numero introducido por ususario es 
+    # 3.1 esta funcion evalua si el numero introducido por ususario es correcto
+    def check(numero):
+        indice
+        posicion
+        for i in range(len(resultado)):
+            for j in range(len(resultado[i])):#va uno por uno
+                if resultado[i][j]== numero:#si existe el numero en el juego
+                    indice= i
+                    posicion = j
+                    intento = True #si sirve el numero para la jugada
+                    return (indice,posicion,intento)# se sale de la f(x)
+                
+        return (indice,posicion,intento) #intento sigue siendo false
 
+    #3.2 Solicitud de informacion y/o numero random
+    while intento == False:
+
+        if jugada % 2 == 0: #Si el numero es par, juega usuario
+            varJugador = "O"
+            lugar = input ("\nIngresa tu movimiento: ")
+            index,pos,intento = check(lugar)
+
+        else:
+            varJugador = "X"
+            lugar= randrange(1,9)
+            print(f"el pc eligió {lugar}")
+            index,pos,intento = check(lugar)
+    
+    #4. Modificará el resultado cuando salga del while
+    resultado[index][pos]= varJugador
